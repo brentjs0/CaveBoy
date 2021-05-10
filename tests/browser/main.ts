500a6bfe5b02c1cd4afcf7c0033f11443627a40b
Brent Seger <brentseger@gmail.com>
2021-05-10 13:29:52 -0500

    Set up in-browser unit testing.

---
+++ tests/browser/main.ts
@@ -0,0 +1,5 @@
+//import '../unit/script/base/CaveBoyImageData.spec';
+import '../unit/script/base/helpers.spec';
+import '../unit/script/data/coilsnake-literal/CoilSnakeMinitilePaletteString.spec';
+import '../unit/script/data/game-object/Color.spec';
+import '../unit/script/data/game-object/MinitilePalette.spec';
